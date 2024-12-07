f5405168c2a73c7e38ad4d32ea1ea84fc44c2923
Anjaneyulu Reddy <anjaneyulureddy64@gmail.com>
2024-12-08 00:17:00 +0530

    New Changes in New project

---
+++ src/ModuleTwo/AudioComponent/index.tsx
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {styles} from '../CssTypes/External/styles';
+
+const AudioComponent = () => {
+  return (
+    <View>
+      <Text>AudioComponent</Text>
+    </View>
+  );
+};
+
+export default AudioComponent;
